7c473cca21bbeafcc229ab077c8b5e523b4f206f
Patrycja Mazur <patrycja.mazur1997@interia.pl>
2023-06-19 19:28:44 +0200

    ...

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from pymongo.cursor import Cursor  # tools for iterating in MongoDB
-from bson.objectid import ObjectId
-from app.models import Coffee
-from app.database import coffees_collection
+from app.routers.coffees import router as coffees_router
 
 description = """
 Coffees API helps you to choice the best coffee to you!
@@ -21,23 +18,4 @@ app = FastAPI(
     contact={"name": "Patrycja", "email": "patrycjamzn@gmail.com"},
 )
 
-
-@app.get("/coffees/", response_model=list[Coffee])
-def get_coffees():
-    coffees = []
-    for coffee in coffees_collection.find():
-        coffee["id"] = str(coffee["_id"])
-        coffees.append(coffee)
-    return coffees
-
-
-@app.post("/coffees/", response_model=Coffee)
-def create_coffee(coffee: Coffee):
-    coffees_collection.insert_one(coffee.dict(exclude_none=True))
-    return coffee
-
-
-@app.delete("/coffees/{id}")
-def delete_coffee(id: str):
-    coffees_collection.delete_one({"_id": ObjectId(id)})
-    return {f"coffee with id {id} delaited from base."}
+app.include_router(coffees_router)
--- app/repository.py
+++ app/repository.py
@@ -10,43 +10,45 @@ from app.models import Coffee
 
 class Repository(ABC):
     @abstractmethod
-    def get_coffees(self, id: str):
+    def get_coffees(self, id:str):
         ...
 
     @abstractmethod
     def created_coffee(self, coffee: Coffee):
         ...
-
+    
     @abstractmethod
-    def deleted_coffee(self, id: str):
+    def deleted_coffee(self, id:str):
         ...
 
     class InMemoryRepository(Repository):
         def __init__(self):
-            self.collection = []
+            self.collection=[]
 
         def _get_next_id(self):
             return uuid4()
-
+        
     class MongoRepository(Repository):
-        def __init__(self, coffees_collection: Collection):
-            self._coffees_collecion = coffees_collection
-
+        def __init__(self, coffees_collection:Collection):
+            self._coffees_collecion=coffees_collection
+        
     def get_coffees(self):
-        coffees: CursorType = self._coffees_collection.find()
+        coffees: CursorType=self._coffees_collection.find()
         coffees = []
         for coffee in self.coffees_collection.find():
-            coffee["id"] = str(coffee["_id"])
-            coffees.append(coffee)
+                coffee["id"] = str(coffee["_id"])
+                coffees.append(coffee)
         return coffees
         raise CoffeeNotFounf(f"Coffee with id '{id}' not found. Try again")
 
-    def create_coffee(self, coffee: Coffee):
-        coffee_id = self._coffees_collection.insert_one(coffee.dict(exclude_none=True))
-        coffee.id = str(coffee_id.inserted_id)
+    def create_coffee(self,coffee: Coffee):
+        coffee_id=self._coffees_collection.insert_one(coffee.dict(exclude_none=True))
+        coffee.id=str(coffee_id.inserted_id)
         return coffee
-
+    
     def delete_coffee(self, id: str):
         self._coffees_collection.delete_one({"_id": ObjectId(id)})
-
+        
         return {f"coffee with id {id} delaited from base."}
+    
+    
